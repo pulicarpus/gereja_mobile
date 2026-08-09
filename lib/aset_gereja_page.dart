25a814d5903ef9e2a970eb3292178041e95dca3d
pulicarpus <pulicarpus@gmail.com>
2026-08-09 17:14:48 +0700

    update lib/aset_gereja_page.dart

--- lib/aset_gereja_page.dart
+++ lib/aset_gereja_page.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'user_manager.dart'; // 👈 PASTIKAN PATH IMPORT INI SESUAI DENGAN LOKASI FILE user_manager.dart ANDA
 
 class AsetGerejaPage extends StatefulWidget {
   final String gerejaId;

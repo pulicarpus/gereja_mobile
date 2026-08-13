f64f7078c2ec10dfc944d9236bd12d60b4e03387
pulicarpus <pulicarpus@gmail.com>
2026-08-13 23:01:28 +0700

    update android/settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,9 +19,9 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     // 👇 GANTI DI SINI: Naikkan ke 8.11.1 sesuai permintaan error build
-    id("com.android.application") version "8.11.1" apply false 
+    id("com.android.application") version "9.0.1" apply false 
     // 👇 Kotlin tetap di 2.1.0 (sudah aman)
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false 
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false 
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
